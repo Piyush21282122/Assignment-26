7b005b6a4844d92809f819b6e4636eeec0ffff7f
Piyush21282122 <108992788+Piyush21282122@users.noreply.github.com>
2022-12-05 12:59:41 +0530

    Add files via upload

--- que1.cpp
+++ que1.cpp
@@ -1,44 +1,46 @@
 
-
 #include<iostream>
 #include<conio.h>
 using namespace std;
-
 class Complex
 {
-    private:
-       int a;
-       int b;
-    public:
-       void setData(int num1,int num2)
-       {
-          a=num1;
-          b=num2;
-       }
-       void showData()
-       {
-          cout<<"real = "<<a<<" img = "<<b;
-          cout<<endl;
-       }
-       Complex add(Complex c)
-       {
-          Complex temp;
-          temp.a=a+c.a;
-          temp.b=b+c.b;
-          return temp;
-       }
+   private:
+      int a; // contains real part
+      int b; // contains Imaginary part
+   public:
+      void setData(int r,int i)
+      {
+         a=r;
+         b=i;
+      }
+      void showData()
+      {
+         cout<<"Real="<<a<<" Imaginary="<<b<<endl;
+      }
+      Complex add(Complex C) // Add To Complex Type Values 
+      {
+         Complex temp;
 
+         temp.a=a+C.a;
+         temp.b=b+C.b;
+
+         return temp;
+      }
 };
 
 int main()
 {
-    Complex c1,c2,c3;
-    c1.setData(2,3);
-    c2.setData(10,20);
+   Complex c1,c2;
+   Complex c3;
 
-    c3=c1.add(c2);
-    
-    c3.showData();
-    getch();
-    return 0;
+   c1.setData(10,22);
+   c2.setData(20,33);
+
+   c3=c1.add(c2);
+
+   c1.showData();
+   c2.showData();
+   c3.showData();
+   getch();
+   return 0;
 }
